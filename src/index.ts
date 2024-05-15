70df2968b11a78963ef5ebf384d647763c451d77
camilomdeoca <camilomdeoca@gmail.com>
2024-05-15 04:13:36 -0300

    Worley noise node

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { ColorCorrectionNode } from "./nodes/filters/colorcorrection.js";
 import { CombineNode } from "./nodes/filters/combine.js";
 import { PerlinNoiseNode } from "./nodes/generators/perlinnoise.js";
 import { SolidColorNode } from "./nodes/generators/solidcolor.js";
+import { WorleyNoiseNode } from "./nodes/generators/worleynoise.js";
 
-export { PerlinNoiseNode, SolidColorNode };
+export { PerlinNoiseNode, WorleyNoiseNode, SolidColorNode };
 export { CombineNode, ColorCorrectionNode };
 export { Color };
---
+++ src/nodes/generators/worleynoise.ts
@@ -0,0 +1,54 @@
+import { Vector2 } from "vectors-typescript";
+import { Color } from "../../color.js";
+import { IGeneratorNode } from "../generatornode.js";
+import { PointGenerationAlgorithm, Worley } from "noises-library";
+
+export class WorleyNoiseNode implements IGeneratorNode {
+  public constructor() {
+    this.recreateWorleyObject();
+  }
+
+  public set seed(seed: string) {
+    this._seed = seed;
+    this.recreateWorleyObject();
+  }
+
+  public get seed(): string {
+    return this._seed;
+  }
+
+  public set numberOfPoints(numberOfPoints: number) {
+    this._numberOfPoints = numberOfPoints;
+    this.recreateWorleyObject();
+  }
+
+  public get numberOfPoints(): number {
+    return this._numberOfPoints;
+  }
+
+  public set pointGenAlgorithm(pointGenAlgorithm: PointGenerationAlgorithm) {
+    this._pointGenAlgorithm = pointGenAlgorithm;
+    this.recreateWorleyObject();
+  }
+
+  public get pointGenAlgorithm(): PointGenerationAlgorithm {
+    return this._pointGenAlgorithm;
+  }
+
+  private recreateWorleyObject() {
+    this._worleyNoise = new Worley({
+      seed: this.seed,
+      numPoints: this.numberOfPoints,
+      pointGenAlgorithm: this.pointGenAlgorithm,
+    });
+  }
+
+  public getValueAt(position: Vector2): Color {
+    return new Color(this._worleyNoise.at(position))
+  }
+
+  private _worleyNoise: Worley;
+  public _seed: string = "defaultseed";
+  public _numberOfPoints: number = 32;
+  public _pointGenAlgorithm: PointGenerationAlgorithm = "halton";
+};
