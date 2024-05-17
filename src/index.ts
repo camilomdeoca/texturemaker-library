618bf5a79c5fd2ffad832d5099699b231f0a6743
camilomdeoca <camilomdeoca@gmail.com>
2024-05-17 17:42:14 -0300

    export nodes interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import { Color } from "./color.js";
+import { IFilterNode } from "./nodes/filternode.js";
 import { BlurNode } from "./nodes/filters/blur.js";
 import { ColorCorrectionNode } from "./nodes/filters/colorcorrection.js";
 import { ColorizeControlPoint, ColorizeNode } from "./nodes/filters/colorize.js";
 import { CombineNode } from "./nodes/filters/combine.js";
 import { WarpNode } from "./nodes/filters/warp.js";
+import { IGeneratorNode } from "./nodes/generatornode.js";
 import { PerlinNoiseNode } from "./nodes/generators/perlinnoise.js";
 import { SolidColorNode } from "./nodes/generators/solidcolor.js";
 import { WorleyNoiseNode } from "./nodes/generators/worleynoise.js";
+import { INode } from "./nodes/node.js";
 
+export { INode, IGeneratorNode, IFilterNode };
 export { PerlinNoiseNode, WorleyNoiseNode, SolidColorNode };
 export { CombineNode, ColorCorrectionNode, ColorizeNode, WarpNode, BlurNode };
 export { ColorizeControlPoint }
